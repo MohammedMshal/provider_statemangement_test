a57cb3afbfbd204a14e864bc41638f922ea4f2d6
MohammedMshal <mohammedmshal2018@gmail.com>
2022-09-10 14:56:35 +0200

    selector provider

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -12,25 +12,44 @@ class HomePage extends StatelessWidget {
           appBar: AppBar(),
           body: Center(
             child: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              child: Consumer<Model>(
-                builder: (context, provider, child) {
-                  return Column(
-                    children: [
-                      Text(provider.name),
-                      const SizedBox(height: 20),
-                      MaterialButton(
-                          color: Colors.blue,
-                          child: const Text('Change'),
-                          onPressed: () {
-                            provider.changeName();
-                            print(provider.name);
-                          })
-                    ],
-                  );
-                },
-              ),
-            ),
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  children: [
+                    Selector<Model, String>(
+                      selector: (context, provider) => provider.name,
+                      builder: (context, value, child) {
+                        return Text(value);
+                      },
+                    ),
+                    Selector<Model, String>(
+                      selector: (context, provider) => provider.name2,
+                      builder: (context, value, child) {
+                        return Text(value);
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    Consumer<Model>(
+                      builder: (context, provider, child) {
+                        return MaterialButton(
+                            color: Colors.blue,
+                            child: const Text('Change'),
+                            onPressed: () {
+                              provider.changeName();
+                            });
+                      },
+                    ),
+                    Consumer<Model>(
+                      builder: (context, provider, child) {
+                        return MaterialButton(
+                            color: Colors.blue,
+                            child: const Text('Change Two'),
+                            onPressed: () {
+                              provider.changeNameTow();
+                            });
+                      },
+                    ),
+                  ],
+                )),
           )),
     );
   }
@@ -38,9 +57,21 @@ class HomePage extends StatelessWidget {
 
 class Model with ChangeNotifier {
   String name = 'Ahmed';
+  String name2 = 'Mohammed';
+
+  get nameONe => name;
+
+  get nameTwo => name2;
 
   void changeName() {
-    name = 'Mohammed';
+    name = 'state changed';
+    print('build Name one');
+    notifyListeners();
+  }
+
+  void changeNameTow() {
+    name2 = 'state changed two';
+    print('build Name tow');
     notifyListeners();
   }
 }
